47fc8c9ec27d83bac70b14817d2937b09fe34726
bzt <vitorcastrobuzato@gmail.com>
2024-03-12 00:33:37 -0300

    mysql introduction

---
+++ node/mySQL/integracao sql node/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+  res.render('home')
+})
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'nodemysql'
+})
+
+conn.connect((err) => {
+  if(err) {
+    console.log(err); 
+    return; 
+  }
+
+  console.log('>> DB on')
+
+  app.listen(3002, () =>{
+    console.log('>> Server on')
+  })
+})
+
