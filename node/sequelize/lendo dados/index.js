318e0285d873ee67ee3bd92d5831e16288a437f9
vbzt <vbzt>
2024-03-18 13:13:06 -0300

    Sequelize

---
+++ node/sequelize/lendo dados/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+
+const User = require('./models/User')
+
+const app = express()
+
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+
+app.get('/users/create', (req, res) => { 
+  res.render('adduser')
+})
+
+
+app.post('/users/create', async (req, res) => { 
+  
+  const name = req.body.name 
+  const occupation = req.body.occupation 
+  let newsletter = req.body.newsletter === "on" ? true : false
+  
+
+  await User.create({name, occupation, newsletter})
+  console.log('>> user criado com nome de ' + name)
+
+  res.redirect('/')
+})
+
+
+app.get('/', async (req, res) =>{
+  const user = await User.findAll({raw: true})
+  res.render('home', {users: user}) 
+})
+
+conn.sync()
+.then(
+  app.listen(3002, () =>{
+  console.log('>> db ok') 
+  console.log('>> server on')
+})).catch((e) => console.log('err during sync'))
