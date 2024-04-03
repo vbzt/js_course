11431f9710dcde91f16cf70e799018956904a683
bzt <vitorcastrobuzato@gmail.com>
2024-04-03 01:37:29 -0300

    Started social media project

---
+++ node/sensus/db/conn.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize({
+  host: 'localhost', 
+  username: 'root',
+  dialect: 'mysql',
+  database: 'sensus'
+  // username: 'postgres',
+  // password: '1qaz2wsx',
+  // dialect: 'postgres'
+  // dialectModule: 'pg'
+})
+
+try{
+  sequelize.authenticate()
+  console.log('>> db ok')
+}catch(e){
+  console.log('>> db err: ' + e)
+}
+
+
+
+module.exports = sequelize        
---
+++ node/sensus/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+
+const app = express() 
+
+const conn = require('./db/conn')
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(
+  session({
+    name: 'session',
+    secret: 'sensus_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function() {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true
+    }
+}))
+
+// flash  
+app.use(flash())
+
+app.use(express.static('public'))
+app.use((req, res, next) => {
+  if(req.session.userid){
+    res.locals.session = req.session
+  }
+  
+  next()
+})
+
+conn.sync()
+.then(() => {
+  app.listen(3002, () => console.log('>> server on'))
+})
+.catch((err) => console.log('>> db err: ' + err))
